3fd2a9469cfd6f857294161a258f5aa7b78e01ef
Ilya M <ky4web@gmail.com>
2016-03-16 18:25:47 +0300

    conf db

--- init.sh
+++ init.sh
@@ -1,4 +1,10 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+
 sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
+
+mysql -uroot -e 'drop database db_mysql'
+mysql -uroot -e 'drop user root'
+mysql -uroot -e "create database db_mysql character set 'UTF8';"
+sudo /etc/init.d/mysql restart
